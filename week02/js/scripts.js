6e07bffde88c309c2c756fe23ae07746938dbc2e
LFernandoPortugal <falconbladex1@gmail.com>
2022-11-09 23:18:33 -0500

    Almost done with Notes for week 02

---
+++ week02/js/scripts.js
@@ -0,0 +1,9 @@
+const x = new Date();
+let year1 = x.getFullYear();
+
+document.getElementById("currentyear").innerHTML = year1;
+
+// console.log(yearm)
+
+let latestDate = document.lastModified
+document.getElementById("lastmodified").innerHTML = latestDate;
